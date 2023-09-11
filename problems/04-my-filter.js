4952524d984769022a06f78b3776254bd5ad51fc
Max Fonda <mxwll155@gmail.com>
2023-09-11 19:23:14 -0400

    PROBLEM 4 DONE

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -20,8 +20,24 @@ console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
 function myFilter(array, cb) {
-    // Your code here 
+    let returnArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (cb(array[i]) === true) {
+            returnArr.push(array[i])
+        }
+    }
+    return returnArr;
 }
+
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
+
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {

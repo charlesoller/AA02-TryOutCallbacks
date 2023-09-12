112d6866a8035a4abc2564a1630bd73f45297af6
charlesoller <charlesrello@gmail.com>
2023-09-12 19:21:25 -0400

    19 DONE

--- problems/19-greater-callback-value.js
+++ problems/19-greater-callback-value.js
@@ -19,9 +19,23 @@ console.log(greaterCallbackValue(9, Math.sqrt, doubler));   // 18
 *******************************************************************************/
 
 function greaterCallbackValue(val, cb1, cb2) {
-  // Your code here 
+  let val1 = cb1(val);
+  let val2 = cb2(val);
+
+  return val1 > val2 ? val1 : val2;
 }
 
+let doubler = function (n) {
+  return 2 * n;
+}
+
+let squarer = function (n) {
+  return n * n;
+}
+
+console.log(greaterCallbackValue(5, doubler, squarer));     // 25
+console.log(greaterCallbackValue(1, doubler, squarer));     // 2
+console.log(greaterCallbackValue(9, Math.sqrt, doubler));   // 18
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = greaterCallbackValue;

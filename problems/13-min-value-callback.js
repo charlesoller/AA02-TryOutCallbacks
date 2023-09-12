fa47f8a38004d26b7132f54b4c69170222f31693
charlesoller <charlesrello@gmail.com>
2023-09-11 20:50:29 -0400

    13 DONE

--- problems/13-min-value-callback.js
+++ problems/13-min-value-callback.js
@@ -11,8 +11,30 @@ console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 *******************************************************************************/
 
 function minValueCallback(array, cb) {
-    // Your code here 
+    let number = 0;
+
+    if(cb === undefined){
+      return Math.min(...array);
+    }
+
+    // } else {
+    //     for (let i = 0; i < array.length; i++){
+    //       number = cb(Math.min(...array))
+    //     }
+    // }
+
+    let num = cb(array[0]);
+    for (let i = 1; i < array.length; i++){
+      if(cb(array[i]) < num){
+        num = cb(array[i])
+      }
+    }
+
+    return num;
 }
+
+console.log(minValueCallback([64, 25, 49, 9, 100]));             // 9
+console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
